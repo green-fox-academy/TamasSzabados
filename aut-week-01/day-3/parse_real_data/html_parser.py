dcf6fbad274ef55297cfe724aff733ce485970f5
Tamas Szabados <tszabad@gmail.com>
2021-02-19 19:53:04 +0100

    aut week 1 day 3 exercise added

---
+++ aut-week-01/day-3/parse_real_data/html_parser.py
@@ -0,0 +1,26 @@
+from lxml import html
+import numpy as np 
+import pandas as pd
+
+with open("leviatan.html", "r",encoding="utf8") as f:
+    tree = html.parse(f)
+    print(tree)
+
+    #getting genres
+    genres = tree.xpath('/html/body/div[2]/div[3]/div[1]/div[2]/div[5]/div[7]/div/div[2]/div/div/div/a/text()')
+print(genres)
+
+for i in range(len(genres)-1):
+    if genres[i] == "Space" and genres[i+1] == "Space Opera":
+        genres.remove(genres[i])
+
+genres_dict ={}
+
+for i in range(0,len(genres)-1,2):
+    genres_dict[genres[i]] = genres[i+1]
+
+genres_series = pd.Series(genres_dict)
+df = pd.DataFrame(genres_series)
+df.to_csv('out.csv')
+
+
