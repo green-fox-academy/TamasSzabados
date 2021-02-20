9e5c8c85b842df989c396064de2e14974f80ff97
Tamas Szabados <tszabad@gmail.com>
2021-02-20 17:24:23 +0100

    reader py imporved

--- aut-week-01/day-2/xml_reader/reader.py
+++ aut-week-01/day-2/xml_reader/reader.py
@@ -8,11 +8,8 @@ with open("food.xml", "r") as f:
     print(elements_price)
 
     #The sum of all the calories
-    elements_calories = tree.xpath('/breakfast_menu/food/calories/text()')
-    total = 0
-    for i in elements_calories:
-        total += float(i)
-    print(total)
+    elements_calories = tree.xpath('sum(/breakfast_menu/foods/calories/text())')
+    print(elements_calories)
 
     #The description of the food with the id: 3
     element_description = tree.xpath('/breakfast_menu/food[@id="3"]/description/text()')
@@ -27,20 +24,20 @@ with open("food.xml", "r") as f:
     print(savoury_desc)
 
     #The count of ingredients of the food with id: 2
-    ingredients_2 = tree.xpath('/breakfast_menu/food[@id="2"]/ingredients/ingredient/type/text()')
-    print(len(ingredients_2))
+    ingredients_2 = tree.xpath('count(//food[@id="2"]/ingredients/ingredient)')
+    print(ingredients_2)
 
     #The second ingredient of "Homestyle Breakfast"
     homestyle = tree.xpath('/breakfast_menu/food[name="Homestyle Breakfast"]/ingredients/ingredient/type/text()')
     print(homestyle[1])
 
     #The count of foods that has "milk" in the ingredients
-    milk_ingr = tree.xpath('/breakfast_menu/food/ingredients/ingredient[type="milk"]/type/text()')
-    print(len(milk_ingr))
+    milk_ingr = tree.xpath('count(/breakfast_menu/food/ingredients/ingredient[type="milk"]/type/text())')
+    print(milk_ingr)
 
     #The count of ingredients of all "sweet" foods
-    sweet_count = tree.xpath('/breakfast_menu/food[@type="sweet"]/ingredients/ingredient/type/text()')
-    print(len(sweet_count))
+    sweet_count = tree.xpath('count(/breakfast_menu/food[@type="sweet"]/ingredients/ingredient/type/text())')
+    print(sweet_count)
 
 
 
