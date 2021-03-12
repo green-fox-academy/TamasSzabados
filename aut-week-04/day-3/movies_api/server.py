f0579b115b2bf5663d564e2e7245a7723dc97088
Tamas Szabados <tszabad@gmail.com>
2021-03-12 19:42:12 +0100

    movies api ready

--- aut-week-04/day-3/movies_api/server.py
+++ aut-week-04/day-3/movies_api/server.py
@@ -1,18 +1,80 @@
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/api/movies', methods=['GET'])
+with open('movies.json') as f:
+  data = json.load(f)
+
+@app.route('/')
+def index():
+    return "<h3> This is my first movie api</h3>"
+
+@app.route('/api/movies', methods=['GET', 'POST'])
 def all_movies():
-    return jsonify()
-
-
- @app.route('/api/movies/<movie_id>', methods=['GET'])
- def movie(movie_id):
-    return jsonify()
+    if request.method == 'GET':
+        return jsonify(data)
+    elif request.method == 'POST':
+        api_key = request.args["API_KEY"]
+        if api_key == "hello":
+            new_dict = {}
+            new_dict["id"] = request.args['id']
+            new_dict["title"] = request.args['title']
+            new_dict["year"] = request.args['year']
+            new_dict["genre"] = request.args['genre']
+            new_dict["description"] = request.args['description']
+            data.append(new_dict)
+            with open('movies.json', 'w') as json_file:
+                json.dump(data, json_file)
+            return jsonify([new_dict]), 200
+        else:
+            return jasonify(["inncorrect api key"]),403
     
+@app.route('/api/movies/<movie_id>', methods=['GET','PUT', 'DELETE'])
+def movie(movie_id):
+    if request.method == 'GET':
+        movie_select = []
+        for i in data:
+            print(i)
+            if i['id'] == int(movie_id):
+                movie_select.append(i)
+        return jsonify(movie_select)
 
-    
+    elif request.method == "PUT":
+        api_key = request.args["API_KEY"]
+        if api_key == "hello":
+            found = False
+            for i in data:
+                if i['id'] == int(movie_id):
+                    found = True
+                    i["title"] = request.args['title']
+                    i["year"] = request.args['year']
+                    i["genre"] = request.args['genre']
+                    i["description"] = request.args['description']
+                    with open('movies.json', 'w') as json_file:
+                        json.dump(data, json_file)
+                    return jsonify([i]), 200
+            if not found:
+                return jsonify([{"error": "No movie found with <movie_id> ID"}]),404
+        else:
+            return jsonify(["inncorrect api key"]),403
+
+    elif request.method == "DELETE":
+        api_key = request.args["API_KEY"]
+        if api_key == "hello":
+            found = False
+            for i in data:
+                if i['id'] == int(movie_id):
+                    found = True
+                    data.remove(i)
+                    with open('movies.json', 'w') as json_file:
+                        json.dump(data, json_file)
+                    return jsonify(["deleted"]), 200
+            if not found:
+                return jsonify([{"error": "No movie found with <movie_id> ID"}]),404
+        else:
+            return jsonify(["inncorrect api key"]),403
+
 
 if __name__ == '__main__':
     app.run(debug=True)
