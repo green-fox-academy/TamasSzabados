ecd0e533eb1049159a5b62078e02e3fb078d6b10
Tamas Szabados <tszabad@gmail.com>
2021-03-16 22:41:36 +0100

    scrapy amazon exeercise added

---
+++ aut_week-05/day-1/amazon/amazon/spiders/amazon_kindle.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class AmazonKindleSpider(scrapy.Spider):
+    name = 'amazon_kindle'
+    allowed_domains = ['https://www.amazon.com']
+    start_urls = ['https://www.amazon.com/s?k=kindle']
+
+    def parse(self, response):
+        product = response.xpath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span/text()").getall()                    
+        price = response.xpath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/div/a/span[1]/span[1]/text()").extract()   
+        ratings = response.xpath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[2]/div/span[2]/a/span/text()").extract()
+
+        print(product)                        
+        print(price )
+        print(ratings)
+        print(len(product))
+        print(len(price))
+        print(len(ratings))
