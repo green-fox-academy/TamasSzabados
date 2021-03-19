bae18d20dee764c25b72504608268a8255909963
Tamas Szabados <tszabad@gmail.com>
2021-03-19 19:07:56 +0100

    amazon book exercise imporved

--- aut_week-05/day-2/amazon2/amazon2/spiders/amazon_book.py
+++ aut_week-05/day-2/amazon2/amazon2/spiders/amazon_book.py
@@ -3,30 +3,29 @@ import scrapy
 
 class AmazonBookSpider(scrapy.Spider):
     name = 'amazon_book'
-    allowed_domains = ['https://www.amazon.com']
+    
     start_urls = ['https://www.amazon.com/s?bbn=283155&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656020011&dc&qid=1615980518&rnid=618072011&ref=lp_1000_nr_p_n_feature_browse-bin_1']
 
     def parse(self, response):
         print("procesing:"+response.url)
-        product = response.xpath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span/text()").getall()
-        price = response.xpath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[2]/div/span[2]/a/span/text()").extract()   
-        ratings = response.xpath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/a/span[1]/span[1]/text()").extract()
-
+        products = response.xpath("//div[@class='s-main-slot s-result-list']")
+        
         print(product)                        
-        print(price )
-        print(ratings)
-        print(len(product))
-        print(len(price))
-        print(len(ratings))
-
-        next_path = response.xpath("//a[@class = 'a-last']/@href").extract_first()
-        if next_path is not None:
-            yield scrapy.Request(response.urljoin(next_path), callback=self.parse)
-
-        for i in range(len(product)):
+      
+        for product in products:
+            
+            rating = product.xpath("//a[@class='a-link-normal']/span[@class='a-size-base' and 1]/text()").get()
+            if rating is None:
+                rating = "not available"
+            
             yield {
-                "product": product[i],
-                "price": price[i],
-                "ratings": ratings[i]
-                    }
+                "product": product.xpath("//span[@class='a-size-medium a-color-base a-text-normal']/text()").get(),
+                "price": product.xpath("//span[@class='a-price-whole']/text()").get(),
+                "ratings": rating
+                }
+            
+
+        next_path = response.xpath("//li[@class='a-last']/a[1]/@href").get()
+        if next_path:
+            yield scrapy.Request(response.urljoin(next_path), callback=self.parse)
            
